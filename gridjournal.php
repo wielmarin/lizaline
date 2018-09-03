27f1121b50e6fd66e921408b05ff24f6a994e318
Marin <wiellersenmarin@gmail.com>
2018-09-03 19:34:06 +0200

    Frontpage grid small page corrections

--- gridjournal.php
+++ gridjournal.php
@@ -7,20 +7,20 @@
 		<p class="grid-journal-right-action">Have a look at our journal</p>
 	</div>
 
-			<a href="<?php the_field('grid_link_6')?>" id="grid-journal-left-top-left" class="grid-box smallsquare hoverunderline" style="background-image:url(<?php the_field('grid_item_6')?>);">
+			<a href="<?php the_field('grid_link_6')?>" id="grid-journal-left-top-left" class="grid-box smallsquare hoverunderline" style="background-image:url(<?php the_field('grid_item_6')?>); background-size: contain">
 				<?php get_template_part('newmarker'); ?>
 				<span id="grid-journal-ltl-description" class="grid-description"><p><?php the_field('grid_description_6')?></p></span>
 				
 				<div id="cta-grid">View product</div>
 			</a>
 			
-			<a href="<?php the_field('grid_link_7')?>" id="grid-journal-left-top-middle" class="grid-box smallsquare hoverunderline"  style="background-image:url(<?php the_field('grid_item_7')?>);">
+			<a href="<?php the_field('grid_link_7')?>" id="grid-journal-left-top-middle" class="grid-box smallsquare hoverunderline"  style="background-image:url(<?php the_field('grid_item_7')?>); background-size: contain">
 				<span id="grid-journal-ltm-description" class="grid-description"><p><?php the_field('grid_description_7')?></p></span>
 				<div id="cta-grid">View product</div>
 				
 			</a>
 
-		<a href="<?php the_field('grid_link_8')?>" id="grid-journal-left-bottom-left" class="grid-box smallsquare hoverunderline" style="background-image:url(<?php the_field('grid_item_8')?>);">
+		<a href="<?php the_field('grid_link_8')?>" id="grid-journal-left-bottom-left" class="grid-box smallsquare hoverunderline" style="background-image:url(<?php the_field('grid_item_8')?>); background-size: contain">
 			<?php get_template_part('salemarker'); ?>
 				<span id="grid-journal-lbl-description" class="grid-description"><p><?php the_field('grid_description_8')?></p></span>
 				<div id="cta-grid">View product</div>
